0cccdd3fec9b09cbf316be97075137cb9b8125f9
Axel <zflii@hotmail.com>
2020-11-27 07:48:40 -0800

    Added 3 function

---
+++ prac_03/temperatures.py
@@ -0,0 +1,31 @@
+MENU = """C - Convert Celsius to Fahrenheit
+F - Convert Fahrenheit to Celsius
+Q - Quit"""
+
+
+def main():
+    print(MENU)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "C":
+            celsius = float(input("Celsius: "))
+            print("Result: {:.2f} F".format(celsius_to_fahrenheit(celsius)))
+        elif choice == "F":
+            fahrenheit = float(input("Fahrenheit : "))
+            print("Result: {:.2f} C".format(fahrenheit_to_celsius(fahrenheit)))
+        else:
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Thank you.")
+
+
+def celsius_to_fahrenheit(celsius):
+    return celsius * 9.0 / 5 + 32
+
+
+def fahrenheit_to_celsius(fahrenheit):
+    return 5 / 9 * (fahrenheit - 32)
+
+
+main()

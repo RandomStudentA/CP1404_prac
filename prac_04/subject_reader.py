c0722f0e285f6fd620d1f29555aca8028d0578f5
Axel <zflii@hotmail.com>
2020-12-05 17:59:39 +0800

    add comment

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -17,6 +17,7 @@ def message_print(input_file):
     for line in input_file:
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
+        # the data is split into 3 parts, the [] calls for certain parts only
         print(parts[0] + "is taught by " + parts[1] + " and has " + parts[2] + " students")
 
 
